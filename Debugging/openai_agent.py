2a7fc3037e529a7b778b8ffc85f89702b8673e55
EngrHashim160 <engrhashim160@gmail.com>
2025-08-16 09:41:39 +0500

    tools concept implemented

--- Debugging/openai_agent.py
+++ Debugging/openai_agent.py
@@ -1,6 +1,8 @@
-from typing import Annotated, TypedDict
+from typing import Annotated
+from typing_extensions import TypedDict
 from langchain_openai import ChatOpenAI
-from langgraph.graph import StateGraph, START, END
+from langgraph.graph import END, START
+from langgraph.graph.state import StateGraph
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode
 from langchain_core.tools import tool
@@ -10,28 +12,58 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class State(TypedDict):
-    messages: Annotated[list[BaseMessage], add_messages]
-    
+os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 
-model = ChatOpenAI(temperature=0)
+os.environ["LANGSMITH_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+
+class State(TypedDict):
+    messages:Annotated[list[BaseMessage],add_messages]
+
+model=ChatOpenAI(temperature=0)
 
 def make_default_graph():
-    workflow = StateGraph(State)
+    graph_workflow=StateGraph(State)
+
+    def call_model(state):
+        return {"messages":[model.invoke(state['messages'])]}
     
-    def call_model(state: State):
-        return {
-            'messages': [model.invoke(state['messages'])]
-        }
-    
-    workflow.add_node('agent', call_model)
-    workflow.add_edge('agent', START)
-    workflow.add_edge('agent', END)
-    
-    agent = workflow.compile()
-    
+    graph_workflow.add_node("agent", call_model)
+    graph_workflow.add_edge("agent", END)
+    graph_workflow.add_edge(START, "agent")
+
+    agent=graph_workflow.compile()
     return agent
 
-agent = make_default_graph()
+def make_alternative_graph():
+    """Make a tool-calling agent"""
 
+    @tool
+    def add(a: float, b: float):
+        """Adds two numbers."""
+        return a + b
+
+    tool_node = ToolNode([add])
+    model_with_tools = model.bind_tools([add])
+    def call_model(state):
+        return {"messages": [model_with_tools.invoke(state["messages"])]}
+
+    def should_continue(state: State):
+        if state["messages"][-1].tool_calls:
+            return "tools"
+        else:
+            return END
+
+    graph_workflow = StateGraph(State)
+
+    graph_workflow.add_node("agent", call_model)
+    graph_workflow.add_node("tools", tool_node)
+    graph_workflow.add_edge("tools", "agent")
+    graph_workflow.add_edge(START, "agent")
+    graph_workflow.add_conditional_edges("agent", should_continue)
+
+    agent = graph_workflow.compile()
+    return agent
+
+agent=make_alternative_graph()
 
